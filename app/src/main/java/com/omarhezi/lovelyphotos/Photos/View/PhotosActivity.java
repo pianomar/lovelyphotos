d3d869574003b1bbcc6f8096e15987196f94ae1e
Omar Hezi <omar.hezi@gmail.com>
2019-03-28 01:02:55 -0300

    Add Photos Activity and layout

---
+++ app/src/main/java/com/omarhezi/lovelyphotos/Photos/View/PhotosActivity.java
@@ -0,0 +1,16 @@
+package com.omarhezi.lovelyphotos.Photos.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.omarhezi.lovelyphotos.R;
+
+public class PhotosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photos);
+    }
+}
